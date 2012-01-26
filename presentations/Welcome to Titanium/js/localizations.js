2029bb656774c6ae8ea8efb38918de54d835d6e3
tonylukasavage <anthony.lukasavage@gmail.com>
2012-01-26 17:22:00 -0500

    added localization files and libraries

---
+++ presentations/Welcome to Titanium/js/localizations.js
@@ -0,0 +1,34 @@
+String.toLocaleString({
+	"en": {
+		"%l_agenda_title": "AGENDA",
+		"%l_agenda1": "What is Titanium?",
+		"%l_agenda2": "How does Titanium work?",
+		"%l_agenda3": "What can Titanium do?",
+		"%l_agenda4": "What's it like developing with Titanium?",
+		"%l_agenda5": "How can I learn more?",
+		"%l_whatis": "What is",
+		"%l_important": "But more importantly...",
+		"%l_why": "Why",
+		"%l_dilemma": "The Dilemma:",
+		"%l_somany": "So many native plaforms...",
+		"%l_sofew": "so few RESOURCES",
+		"%l_platform": "Platform",
+		"%l_crossplatform":''
+	},
+	"es": {
+		"%l_agenda_title": "ORDEN DEL DÍA",
+		"%l_agenda1": "¿Qué es el Titanium?",
+		"%l_agenda2": "¿Cómo Titanium de trabajo?",
+		"%l_agenda3": "¿Qué se puede hacer Titanium?",
+		"%l_agenda4": "¿Cómo es el desarrollo con el Titanium?",
+		"%l_agenda5": "¿Cómo puedo aprender más?",
+		"%l_whatis": "¿Qué es el",
+		"%l_important": "Pero lo más importante...",
+		"%l_why": "¿Por qué",
+		"%l_dilemma": "El Dilema:",
+		"%l_somany": "Plataformas nativas tantos...",
+		"%l_sofew": "tan pocos RECURSOS",
+		"%l_platform": "Plataforma"
+		
+	}
+});
