74e158459ac0f4e294e8528c02fe53c03cba1ecb
tonylukasavage <anthony.lukasavage@gmail.com>
2012-02-01 18:16:23 -0500

    initial import of presentation template

---
+++ presentations/template/js/localizations.js
@@ -0,0 +1,10 @@
+String.toLocaleString({
+	"en-US": {
+		"%l_hereissometext": "Here is some text that needs styling",
+		"%l_contact": "Contact"
+	},
+	"es": {
+		"%l_hereissometext": "Aquí es un texto que las necesidades de diseño",
+		"%l_contact": "Contacto"
+	}
+});
