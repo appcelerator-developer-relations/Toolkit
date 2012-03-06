2bb049af9f0ee141c9dfd4514a7ef284b25bf4bc
Kevin Whinnery <kevin.whinnery@gmail.com>
2012-03-06 16:53:09 -0600

    adding Procfile

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ app.configure('production', function(){
 	app.use(express.errorHandler()); 
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
