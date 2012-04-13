435ebb93151d5b1872edde1171c2b383fab24f0b
tonylukasavage <anthony.lukasavage@gmail.com>
2012-04-13 14:08:47 -0400

    initial import of web-vs_native

---
+++ presentations/web_vs_native/js/localizations.js
@@ -0,0 +1,12 @@
+String.toLocaleString({
+	"en-US": {
+		"%l_appcnews": "Appcelerator News",
+		"%l_qa": "Q&A",
+		"%l_thanks": "Thank You!"
+	},
+	"es": {
+		"%l_appcnews": "Appcelerator Noticias",
+		"%l_qa": "Q&A",
+		"%l_thanks": "¡Gracias!"
+	}
+});
